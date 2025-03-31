b061e640d58f9dd4151fdd7a961e24b25461207a
coderfake <hoangdieu22022002@gmail.com>
2025-04-01 01:12:43 +0700

    update: update routing

--- PongGame/asgi.py
+++ PongGame/asgi.py
@@ -1,35 +1,19 @@
-"""
-ASGI config for PongGame project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
+# PongGame/asgi.py
 import os
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
-from game.consumers import GameConsumer
-from chat.consumers import ChatConsumer, DirectChatConsumer
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PongGame.settings")
 
-django_asgi_app = get_asgi_application()
-
-import PongGame.routing
+from PongGame.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
-            URLRouter(
-                PongGame.routing.websocket_urlpatterns
-            )
+            URLRouter(websocket_urlpatterns)
         )
     ),
 })
